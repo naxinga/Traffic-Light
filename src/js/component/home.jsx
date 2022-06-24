6082b8151bf8a04b56173c2f6b9cc60078ef47f5
naxinga <ngarconf@hotmail.com>
2022-06-24 18:45:03 +0000

    deApagadoALuz

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,21 +11,15 @@ const Home = () => {
 		<>
 		<div className="soporte"></div>
 		<div className="semaforo">
+		<div
+			onClick={() => setSelectLight("red")}
+			className={"light" +(selectLight == "red" ? " seleccionado  redSel" : " red")}></div>
 			<div
-			onClick={() => setSelectLight ("red")}
-			className={
-				"light red" +(selectLight == "red" ? " encender" : "")
-			}></div>
+			onClick={() => setSelectLight("yellow")}
+			className={"light" +(selectLight == "yellow" ? " seleccionado yellowSel" : " yellow")}></div>
 			<div
-			onClick={() => setSelectLight ("yellow")}
-			className={
-				"light yellow" +(selectLight == "yellow" ? " encender" : "")
-			}></div>
-			<div
-			onClick={() => setSelectLight ("green")}
-			className={
-				"light green" +(selectLight == "green" ? " encender" : "")
-			}></div>
+			onClick={() => setSelectLight("green")}
+			className={"light" +(selectLight == "green" ? " seleccionado greenSel" : " green")}></div>
 		</div>
 		</>
 	);
